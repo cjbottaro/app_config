2e2443b46a2a7e33181e0dc059d04aea2c07ad3a
Fred Wu <canglan@gmail.com>
2010-05-25 20:32:52 +1000

    Rails compatibility.

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require 'app_config'
 
-::AppConfig = ApplicationConfiguration.new(RAILS_ROOT+"/config/app_config.yml",
-                                           RAILS_ROOT+"/config/environments/#{RAILS_ENV}.yml")
+::AppConfig = ApplicationConfiguration.new("#{Rails.root}/config/app_config.yml",
+                                           "#{Rails.root}/config/environments/#{Rails.env}.yml")
